e967a46b5483d8778a89757d724d98f0650e5855
delano <delano@solutious.com>
2009-06-29 10:36:29 -0400

    Added tryouts for uniqueness (#hash vs #gibbler)

--- tryouts/performance_tryouts.rb
+++ tryouts/performance_tryouts.rb
@@ -1,8 +1,8 @@
 
-library :gibbler2, File.dirname(__FILE__), '..', 'lib'
+library :gibbler, File.dirname(__FILE__), '..', 'lib'
 group "Performance"
 
-tryouts "Object#gibbler", :benchmark do
+tryouts "Speed", :benchmark do
   
   drill "Setup variables" do
     @@array = (1..10000).map { rand }
@@ -11,20 +11,65 @@ tryouts "Object#gibbler", :benchmark do
     @@hash = Hash[zipped]
   end
   
-  dream :ne, Object
-  drill "gibbler", 15 do
+  drill "Array#hash", 15 do
+    @@array.hash
+  end 
+  drill "Array#gibbler", 15 do
     @@array.gibbler
   end
   
-  drill"hash", 15 do
-    @@array.hash
-  end 
+  drill "Hash#hash", 15 do
+    @@hash.hash
+  end
+  drill "Hash#gibbler", 15 do
+    @@hash.gibbler
+  end
   
-
 end
 
+repetitions = 100         # at 100_000 hash shows errors
+sample_size = 1..100
 
-tryouts "Object#hash", :benchmark do
+tryouts "Uniqueness", :api do
+  
+  drill "Array#hash, all unique", 0 do
+    seen = []
+    repetitions.times do
+      srand
+      seen << ((sample_size).map { rand }).hash
+    end
+    seen.size - seen.uniq.size
+  end
+  
+  drill "Hash#hash, all unique", 0 do
+    seen = []
+    repetitions.times do
+      srand
+      seen << Hash[(sample_size).map { rand }.zip((sample_size).map { rand })].hash
+    end
+    seen.size - seen.uniq.size
+  end
+  
+  drill "Array#gibbler, all unique", 0 do
+    seen = []
+    repetitions.times do
+      srand
+      seen << ((sample_size).map { rand }).gibbler
+    end
+    seen.size - seen.uniq.size
+  end
+  
+  drill "Hash#gibbler, all unique", 0 do
+    seen = []
+    repetitions.times do
+      srand
+      seen << Hash[(sample_size).map { rand }.zip((sample_size).map { rand })].gibbler
+    end
+    seen.size - seen.uniq.size
+  end
+  
+  
+  
   
   
 end
