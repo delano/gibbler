f5c2bc0225294568bb85f3dacb60bc3947496e11
delano <delano@solutious.com>
2010-07-18 10:06:10 -0400

    More tryouts converstion; renamed tryouts dir to try

---
+++ try/10_basic_try.rb
@@ -0,0 +1,135 @@
+require 'gibbler'
+
+#Gibbler.enable_debug
+
+# include Gibbler raises exception
+begin
+  a = Class.new
+  a.send :include, Gibbler
+rescue RuntimeError
+  :success
+end
+#=> :success
+
+# Symbol digests are consistent
+:kimmy.gibbler
+#=> '52be7494a602d85ff5d8a8ab4ffe7f1b171587df' 
+
+# String digests are consistent
+'kimmy'.gibbler
+#=> 'c8027100ecc54945ab15ddac529230e38b1ba6a1'
+
+# String and Symbol return different digests
+:kimmy.gibbler != "kimmy".gibbler
+#=> true
+
+# Class digests are consistent
+Class.gibbler
+#=> '25ac269ae3ef18cdb4143ad02ca315afb5026de9'
+
+# Fixnum instance digests are consistent
+1.gibbler
+#=> 'a9cad665549bd22a4346fcf602d9d3c3b0482bbe'
+
+# Bignum instance
+100000000000.gibbler
+#=> '608256db120251843843bba57e9b2c7adb7342aa'
+
+# Empty Hash instance digests are consistent
+Hash.new.gibbler
+#=> '4fdcadc66a38feb9c57faf3c5a18d5e76a6d29bf'
+  
+# Populated Hash instance
+{ :a => [1,2,3, [4,5,6]], :b => { :c => Class } }.gibbler
+#=> "1d4b62e1e9f2c097b0cefb6877bf47c2015cdd21"
+  
+# Empty Array instance
+Array.new.gibbler
+#=> '48fda57c05684c9e5c3259557851943572183a21' 
+
+# Populated Array instance
+[1, 2, :runtime, [3, "four", [Object, true]]].gibbler
+#=> "3e1d79d113a409a96a13ca3879fc4c42027aa74b"
+
+# Knows when a Hash has not changed
+a = { :magic => true }
+a.gibbler
+a[:magic] = true
+a.gibbled?
+# => false
+
+# Knows when a Hash has changed
+a = { :magic => true }
+a.gibbler
+a[:magic] = false
+a.gibbled?
+#=> true
+
+# Two Symbol digests don't cross streams
+a, b = :something, :anything
+a.gibbler
+b.gibbler
+[a.gibbler_cache.short, b.gibbler_cache.short]
+#=> ["667ce086", "92d5f7cd"]
+
+# Two String digests don't cross streams" do
+a, b = 'something', 'anything'
+a.gibbler
+b.gibbler
+[a.gibbler_cache.short, b.gibbler_cache.short]
+#=> ["ce0c7694", "c13b2f02"]
+
+## DISABLED: If gibbler/history is required, there will be an
+## additional attic_var (:gibbler_history), but only if the
+## gibbler_history method has been called already (the history
+## remains nil by default). The fix is not straightfroward and
+## tests are not important anyway so disabling them is fine. 
+## Symbol has list of attic vars", [:gibbler_cache] do
+#  Symbol.attic_vars
+#end
+#
+## String has list of attic vars", [:gibbler_cache] do
+#  String.attic_vars
+#end
+#
+## Hash has list of attic vars", [:gibbler_cache] do
+#  Hash.attic_vars
+#end
+
+# Freezing an object will update the digest
+  a = { :a => 1 }
+  pre  = a.gibbler; 
+  a[:a] = 2 
+  post = a.freeze.gibbler
+  pre != post && post == a.gibbler_cache
+#=> true
+
+# works on arbitrary objects" do
+class ::FullHouse
+  include Gibbler::Complex
+  attr_accessor :roles
+end
+a = FullHouse.new
+a.roles = [:jesse, :joey, :danny, :kimmy, :michelle, :dj, :stephanie]
+a.gibbler
+#=> "fa5f741275b6b27932537e1946042b0286286e1d"
+
+# TODO: Update for Attic 0.4
+# doesn't reveal @__gibbler_digest__ instance variable", false do
+  a = {}
+  a.gibbler  # We need to gibbler first so it sets a value to the instance var
+  val = RUBY_VERSION >= '1.9' ? :'@__gibbler_digest__' : '@__gibbler_digest__'
+  a.instance_variables.member? val
+#=> false
+
+# previous digest"
+  a = "kimmy"
+  a.gibbler
+  a.gibbler_cache
+#=> 'c8027100ecc54945ab15ddac529230e38b1ba6a1'
+
+
+
+
+
+
---
+++ try/14_extended_try.rb
@@ -0,0 +1,80 @@
+require 'gibbler'
+
+Gibbler.digest_type = Digest::SHA1
+
+## true can gibbler
+true.gibbler
+#=> 'f2b0150c84c5c834406ec9cdec989a0fa938b4ad' 
+
+# false can gibbler
+false.gibbler
+#=> 'abee839edf5f9c101c505c28987ca35c31c7fc8d' 
+
+# TrueClass can gibbler
+TrueClass.gibbler
+#=> '583fb214ec50d2c4a123cc52de0c65e801d13516' 
+
+# FalseClass can gibbler
+FalseClass.gibbler
+#=> '11f262be475ddf38a25888e9f6ec82f384a7c58b' 
+
+# Class can gibbler
+Class.gibbler
+#=> '25ac269ae3ef18cdb4143ad02ca315afb5026de9' 
+
+# Object can gibbler
+Object.gibbler
+#=> '5620e4a8b10ec6830fece61d33f5d3e9a349b4c2' 
+
+# Class instance can gibbler
+Class.new.gibbler
+#=> '083feec632e6cd5347e3fb3c7048365c3a0d710e' 
+
+# Module can gibbler
+Module.gibbler
+#=> '7295241e929ffd7cc974cf8d4481291e070937fc' 
+
+# Module instance can gibbler
+Module.new.gibbler
+#=> '6b5a192fd377dfc5c2828a3ad6105b68b6db33d5' 
+
+# Proc can gibbler"
+Proc.gibbler
+#=> '8640f7abcbcb80e3825ed827bf36819e26119e16' 
+
+# Range instance (..) can gibbler
+(1..100).gibbler
+#=> '5450635218fd16e594976e87abd0955529e28248' 
+
+# Range ... is different than ..
+(1...100).gibbler != (1..100).gibbler
+#=> true
+
+# Range instance with Floats can gibbler
+(0.1...1.5).gibbler
+#=> '9d3cd5a21c17cfa0b4bb685730bd07994c486ba2' 
+
+# Range instance with one Float can gibbler
+(0.1..1).gibbler
+#=> 'c725659ca0f3fa3c9ab807bb2db4b718e2dca042' 
+
+# Range (..) doesn't equal range (...)"
+('a'..'e').gibbler != ('a'...'e').gibbler
+#=> true
+
+# nil has a gibbler
+nil.gibbler
+#=> '06fdf26b2a64e90cd35ea9162d9cc48c9f6bb13c'
+
+# Module can gibbler
+Module.gibbler
+#=> '7295241e929ffd7cc974cf8d4481291e070937fc' 
+
+# Module instance can gibbler
+Module.new.gibbler
+#=> '6b5a192fd377dfc5c2828a3ad6105b68b6db33d5' 
+
+# Class can gibbler
+Class.gibbler
+#=> '25ac269ae3ef18cdb4143ad02ca315afb5026de9' 
+
